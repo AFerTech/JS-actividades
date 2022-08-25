9bb77c9c027ec04375437a20f90d8132a5aaf140
Alexis Fernando <alexis96fer@gmail.com>
2022-08-25 04:05:10 -0500

    primer actividad

---
+++ lista.js
@@ -0,0 +1,16 @@
+
+
+const listaYo = [
+    "Alexis",
+     26, 
+     true,
+     new Date(1996,06,15),
+     libro={
+        titulo: "Si lo crees, lo creas",
+        autor: "Brian Tracy",
+        fecha: new Date(2021,02,13),
+        url: "https://www.amazon.com.mx/Si-crees-creas-creencias-alcanzar/dp/607380010X/ref=asc_df_607380010X/?tag=gledskshopmx-20&linkCode=df0&hvadid=450954662337&hvpos=&hvnetw=g&hvrand=12978541593421009760&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1010196&hvtargid=pla-1203354765533&psc=1"
+     }
+    ];
+
+    console.log(listaYo);
